0b55ae8b865903ba71ee89cc20976b4140829b5e
lichaogit <lichaozhaoreg@gmail.com>
2025-04-28 23:40:02 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1291,7 +1291,10 @@ async function 生成配置信息(userID, hostName, sub, UA, RproxyIP, _url, env
 			addresses = addresses.concat('127.0.0.1:1234#CFnat');
 			if (hostName.includes(".workers.dev")) {
 				addressesnotls = addressesnotls.concat(cfips.map(cidr => generateRandomIPFromCIDR(cidr) + '#CF随机节点'));
+				addressesnotls = addressesnotls.concat(cfips.map(cidr => generateRandomIPFromCIDR(cidr) + '#CF随机节点'));
 			} else {
+				addresses = addresses.concat(cfips.map(cidr => generateRandomIPFromCIDR(cidr) + '#CF随机节点'));
+				addresses = addresses.concat(cfips.map(cidr => generateRandomIPFromCIDR(cidr) + '#CF随机节点'));
 				addresses = addresses.concat(cfips.map(cidr => generateRandomIPFromCIDR(cidr) + '#CF随机节点'));
 			}
 		}
